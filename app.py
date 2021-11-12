89fb015a54596cf8d0ff8a1190ae30f62eeca95d
Adhitya Venkatesh <avenkate@alumni.stanford.edu>
2021-11-12 00:04:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,42 @@ import numpy as np
 import pickle
 import sklearn
 
+VALID_PGA_ROUNDS = {1, 2}
+VALID_EUR_ROUNDS = {3, 4}
+EUR_MODEL_LOC = 'svm_ror_EUR_R[3, 4]_2b_all_data.sav'
+PGA_MODEL_LOC = 'svm_ror_PGA_R[1, 2]_3b_all_data.sav'
+
 st.title('ML Golf Betting')
 
 tournament = st.radio("Select the tournament", ["PGA 3-Ball", "EUR 2-Ball"])
 round = st.radio("Select the round", ["1", "2", "3", "4"])
 
-load_model = st.text('Loading Model...')
-load_model = load_model.text('Done!')
+load_data_state = st.text('Loading Data...')
 
-'''
 @st.cache
 def load_data(tourney):
-    pass
+    if tourney == "PGA 3-Ball":
+        return pd.read_csv('Streamlit_3ball_classification.csv')
+    else:
+        return pd.read_csv('Streamlit_2ball_classification.csv')
+
+data = load_data(tournament)    
+load_data_state = load_data_state.text('Done loading data!')
+load_model_state = st.text('Loading Model...')
 
 @st.cache
 def load_model(tourney, rnd):
-    if tourney == "PGA 3-Ball" and rnd == "EUR 2-Ball":
-        
-        
+    if tourney == "PGA 3-Ball" and rnd in VALID_PGA_ROUNDS:
+        return pickle.load(open(PGA_MODEL_LOC, 'rb'))
+    elif tourney == "EUR 2-Ball" and rnd in VALID_EUR_ROUNDS:
+        return pickle.load(open(EUR_MODEL_LOC, 'rb'))
+    else
+        st.text('Sorry, please select round 1 or 2 in PGA and round 3 or 4 in EUR')
 
+model = load_model(tournament, round)    
+load_model_state = load_model_state.text('Done loading model!')
+
+'''
 scaler = StandardScaler()
 X_train_norm = scaler.fit_transform(X_train)   
 X_cv_norm = scaler.transform(X_cv)
